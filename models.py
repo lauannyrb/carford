ffd9947ecaf8607afb13b5ad339bba53dff0d84f
lauannyrb <lauannyrodrigues05@gmail.com>
2024-06-16 16:16:40 -0300

    formatted code with blue

--- models.py
+++ models.py
@@ -2,11 +2,11 @@ from sqlmodel import Field, SQLModel
 
 
 class OwnerModel(SQLModel, table=True):
-    
+
     __tablename__ = 'owners'
-    
+
     id: int | None = Field(default=None, primary_key=True)
-    name: str 
+    name: str
 
 
 class CarModel(SQLModel, table=True):
@@ -15,13 +15,14 @@ class CarModel(SQLModel, table=True):
 
     id: int | None = Field(default=None, primary_key=True)
     color: str
-    model: str 
+    model: str
     owner_id: int = Field(foreign_key='owners.id')
 
-class UserModel(SQLModel,table=True ):
-    
+
+class UserModel(SQLModel, table=True):
+
     __tablename__ = 'users'
-    
+
     id: int | None = Field(default=None, primary_key=True)
     username: str
     password: str
--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ from flask import abort, make_response
 
 
 def abort_request(message, status_code):
-    respose = make_response({'message':message}, status_code)
+    respose = make_response({'message': message}, status_code)
     abort(respose)
